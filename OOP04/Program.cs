09a99d508f4e1bbffc2fda13ee5cd6c024600895
Mohab662 <mohabmbelkan@gmail.com>
2024-07-18 02:54:19 +0300

    Deep Copy Vs Shallow Copy [Array Of Reference Type (StringBuilder)]

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OOP04.Interfaces;
+using System.Text;
 
 namespace OOP04
 {
@@ -71,14 +72,100 @@ namespace OOP04
 
             //Arr02[0] = 100;
             //Console.WriteLine(Arr02[0]);
+
+            #endregion
+
+
+            #endregion
+
+
+            #region Deep Copy Vs Shallow Copy [Array Of Reference Type (String)]
             
+            #region Shallow Copy
+            //string[] Name01 = new string[] { "mohab " };
+            //string[] Name02 = new string[1];
+
+            //Console.WriteLine($"HashCode Arr01 = {Name01.GetHashCode()}");
+            //Console.WriteLine($"HashCode Arr02 = {Name02.GetHashCode()}");
+
+            ////// Shallow Copy
+            //Name02 = Name01;
+            //Console.WriteLine("after shallow copy");
+            //Console.WriteLine($"HashCode Arr01 = {Name01.GetHashCode()}");
+            //Console.WriteLine($"HashCode Arr02 = {Name02.GetHashCode()}");
+
+            //Console.WriteLine(Name01[0]); // mohab
+            //Console.WriteLine(Name02[0]); // mohab
+
+            //Name02[0] = "Mohamed";
+            //Console.WriteLine("after changing");
+            //Console.WriteLine($"HashCode Arr01 = {Name01[0]}");
+            //Console.WriteLine($"HashCode Arr02 = {Name02[0]}");
             #endregion
 
+            #region Deep Copy
+            //Name02 = (string[])Name01.Clone();
+
+            //Console.WriteLine("after Deep copy");
+            //Console.WriteLine($"HashCode Arr01 = {Name01.GetHashCode()}");
+            //Console.WriteLine($"HashCode Arr02 = {Name02.GetHashCode()}");
+
+            //Console.WriteLine(Name01[0]); // Mohamed
+            //Console.WriteLine(Name02[0]); // Mohamed
+
+            //Name02[0] = "Bekan";
+            //Console.WriteLine("after changing");
+            //Console.WriteLine($"HashCode Arr01 = {Name01[0]}"); // Mohamed
+            //Console.WriteLine($"HashCode Arr02 = {Name02[0]}"); // Belkan
 
             #endregion
 
+            #endregion
 
+            #region Part 05 Deep Copy Vs Shallow Copy [Array Of Reference Type (StringBuilder)]
 
+            //StringBuilder[] Name01 = new StringBuilder[1];
+
+            //Name01[0] = new StringBuilder ( "mohab" );
+
+            //StringBuilder[] Name02 = new StringBuilder[1];
+
+            #region Shallow Copy
+            //Console.WriteLine($"HashCode Arr01 = {Name01.GetHashCode()}");
+            //Console.WriteLine($"HashCode Arr02 = {Name02.GetHashCode()}");
+
+            //Console.WriteLine(Name01[0]);
+            //Console.WriteLine(Name02[0]);
+
+            //////// Shallow Copy
+            //Console.WriteLine("after shallow copy");
+            //Console.WriteLine($"HashCode Arr01 = {Name01.GetHashCode()}");
+            //Console.WriteLine($"HashCode Arr02 = {Name02.GetHashCode()}");
+            //Console.WriteLine(Name01[0]);
+            //Console.WriteLine(Name02[0]);
+
+            //Name02[0].Append("Nagy");
+            //Console.WriteLine("after change");
+            //Console.WriteLine($"HashCode Arr01 = {Name01.GetHashCode()}");
+            //Console.WriteLine($"HashCode Arr02 = {Name02.GetHashCode()}");
+
+            #endregion
+
+            #region Deep copy
+            //Name02 = (StringBuilder[])Name01.Clone();
+            //Console.WriteLine("after Deep copy");
+            //Console.WriteLine($"HashCode Arr01 = {Name01.GetHashCode()}");
+            //Console.WriteLine($"HashCode Arr02 = {Name02.GetHashCode()}");
+            //Console.WriteLine(Name01[0]);
+            //Console.WriteLine(Name02[0]);
+
+            //Name02[0].Append("Nagy");
+            //Console.WriteLine("after change");
+            //Console.WriteLine($"HashCode Arr01 = {Name01.GetHashCode()}");
+            //Console.WriteLine($"HashCode Arr02 = {Name02.GetHashCode()}"); 
+            #endregion
+
+            #endregion
         }
     }
 }
