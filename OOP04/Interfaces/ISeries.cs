b8154add2699718688b1db9d832b039310f44a22
Mohab662 <mohabmbelkan@gmail.com>
2024-07-18 00:42:02 +0300

    Interface - Example 02

---
+++ OOP04/Interfaces/ISeries.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP04.Interfaces
+{
+    internal interface ISeries
+    {
+        // 1. Signature For Property
+        public int Current { get; set; }
+
+        // 1. Signature For Methods
+        public void GetNext();
+        public void Reset()
+        {
+            Current = 0;
+        }
+    }
+}
--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace OOP04
+﻿using OOP04.Interfaces;
+
+namespace OOP04
 {
     internal class Program
     {
+        public static void Print10NumberFromSeries(ISeries series)
+        {
+            if (series==null)
+            {
+                return;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{series.Current}\t");
+                series.GetNext();
+            }
+            series.Reset();
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Interface - Example 02
+            //SeriesByTwo seriesByTwo = new SeriesByTwo();
+            //SeriesByThree seriesByThree = new SeriesByThree();
+            //Print10NumberFromSeries(seriesByTwo);
+            //Print10NumberFromSeries(seriesByThree); 
+            #endregion
         }
     }
 }
