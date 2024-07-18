45c29d57e25dbcbc4a006f2f27948c22b04be78c
Mohab662 <mohabmbelkan@gmail.com>
2024-07-19 00:37:31 +0300

    Question 01

---
+++ OOP04/Interfaces Q01/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP04.Interfaces_Q01
+{
+    internal class Rectangle : IRectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Rectangle(double _width, double _height)
+        {
+            Height = _height;
+            Width = _width;
+        }
+        public double Area { get { return Width * Height; } }
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Width = {Width}\nHieght = {Height}\nArea = {Area}");
+        }
+    }
+}
--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -1,5 +1,7 @@
 ﻿using OOP04.IClonable;
 using OOP04.Interfaces;
+using OOP04.Interfaces_EX2;
+using OOP04.Interfaces_Q01;
 
 namespace OOP04
 {
@@ -21,6 +23,7 @@ namespace OOP04
         }
         static void Main(string[] args)
         {
+            #region Part01
             #region Interface - Example 02
             //SeriesByTwo seriesByTwo = new SeriesByTwo();
             //SeriesByThree seriesByThree = new SeriesByThree();
@@ -225,10 +228,69 @@ namespace OOP04
             //    Console.WriteLine(item);
             //} 
             #endregion
+            #endregion
+
+            /////////Part 02 Choose
+            #region Part 02
+
+            #region Part 02 Choose
+            //Question 1: What is the primary purpose of an interface in C#?
+            // a) To provide a way to implement multiple inheritance
+
+            //Question 2: Which of the following is NOT a valid access modifier for interface members in C#?
+            // a) private
+
+            //Question 3: Can an interface contain fields in C#?
+            // a) Yes
+
+            //Question 4: In C#, can an interface inherit from another interface?
+            // b) Yes, interfaces can inherit from multiple interfaces
+
+            //Question 5: Which keyword is used to implement an interface in a class in C#?
+            // d) implements
+
+            //Question 6: Can an interface contain static methods in C#?
+            // a) Yes
+
+            //Question 7: In C#, can an interface have explicit access modifiers for its members?
+            // b) No, all members are implicitly public
+
+            //Question 8: What is the purpose of an explicit interface implementation in C#?
+            // a) To hide the interface members from outside access
+
+            //Question 9: In C#, can an interface have a constructor?
+            // b) No, interfaces cannot have constructors
+
+            //Question 10: How can a C# class implement multiple interfaces?
+            // c) By separating interface names with commas 
+            #endregion
+
+            #region Part 02 Interfaces
+
+            #region Question 01:
+
+            //Circle circle = new Circle(10);
+            //circle.DisplayShapeInfo();
+
+            //Rectangle rectangle = new Rectangle(10, 20);
+            //rectangle.DisplayShapeInfo();
+
+            #endregion
+
+            #region Question 02:
+
+
+
+
+            #endregion
+
+            #endregion
+
+            #endregion
 
 
 
         }
-          
+
     }
 }
